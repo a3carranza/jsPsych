aa52f5d8f22ade158f6c2c21d442dd5612e0ea25
a3carranza <149541252+a3carranza@users.noreply.github.com>
2023-11-10 09:46:40 -0800

    Add files via upload

---
+++ dist/distractor.js
@@ -0,0 +1,78 @@
+var distractor = [
+  {
+    "Problem": "15 + 12",
+    "Answer": "27"
+  },
+  {
+    "Problem": "13+ 5",
+    "Answer": "18"
+  },
+  {
+    "Problem": "8 + 8",
+    "Answer": "16"
+  },
+  {
+    "Problem": "23 + 49",
+    "Answer": "72"
+  },
+  {
+    "Problem": "321 + 432",
+    "Answer": "753"
+  },
+  {
+    "Problem": "19 + 7",
+    "Answer": "26"
+  },
+  {
+    "Problem": "23 + 78",
+    "Answer": "101"
+  },
+  {
+    "Problem": "54 + 3",
+    "Answer": "57"
+  },
+  {
+    "Problem": "4 + 10",
+    "Answer": "14"
+  },
+  {
+    "Problem": "16 + 34",
+    "Answer": "50"
+  },
+  {
+    "Problem": "67 + 2",
+    "Answer": "69"
+  },
+  {
+    "Problem": "235 + 354",
+    "Answer": "589"
+  },
+  {
+    "Problem": "18 + 14",
+    "Answer": "32"
+  },
+  {
+    "Problem": "7 + 93",
+    "Answer": "100"
+  },
+  {
+    "Problem": "14 + 68",
+    "Answer": "82"
+  },
+  {
+    "Problem": "78 + 0",
+    "Answer": "78"
+  },
+  {
+    "Problem": "85 + 12",
+    "Answer": "97"
+  },
+  {
+    "Problem": "76 + 48",
+    "Answer": "124"
+  },
+  {
+    "Problem": "22 + 16",
+    "Answer": "38"
+  }
+]
