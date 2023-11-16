1dec236b6766f78d962f5ed86f0995ad889d6f26
a3carranza <149541252+a3carranza@users.noreply.github.com>
2023-11-16 14:14:19 -0800

    Add files via upload

--- dist/distractor.js
+++ dist/distractor.js
@@ -46,5 +46,53 @@ var distractor = [
   {
     "Problem": "35 - 35",
     "Answer": "0"
+  },
+  {
+    "Problem": "258 - 249",
+    "Answer": "9"
+  },
+  {
+    "Problem": "-23 + 31",
+    "Answer": "8"
+  },
+  {
+    "Problem": "-11 + 14",
+    "Answer": "3"
+  },
+  {
+    "Problem": "87-79",
+    "Answer": "8"
+  },
+  {
+    "Problem": "43 - 34",
+    "Answer": "9"
+  },
+  {
+    "Problem": "-7 + 13",
+    "Answer": "6"
+  },
+  {
+    "Problem": "98 - 93",
+    "Answer": "5"
+  },
+  {
+    "Problem": "-2 + 3",
+    "Answer": "1"
+  },
+  {
+    "Problem": "-31 + 33",
+    "Answer": "2"
+  },
+  {
+    "Problem": "368 - 366",
+    "Answer": "2"
+  },
+  {
+    "Problem": "-15 + 18",
+    "Answer": "3"
+  },
+  {
+    "Problem": "999-994",
+    "Answer": "5"
   }
 ]
