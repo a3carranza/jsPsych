d68b6608ee74320fe88bf4f6a0b286c0847bc9c0
a3carranza <149541252+a3carranza@users.noreply.github.com>
2023-11-21 10:00:44 -0800

    Add files via upload

---
+++ dist/80_pilot.js
@@ -0,0 +1,762 @@
+var wordpair = [
+  {
+    "": 1,
+    "Cue": "MUD",
+    "Target": "PIG",
+    "Paired": "MUD - PIG",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 2,
+    "Cue": "AVENUE",
+    "Target": "PARK",
+    "Paired": "AVENUE - PARK",
+    "Q1": 0,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 3,
+    "Cue": "WINE",
+    "Target": "CHEESE",
+    "Paired": "WINE - CHEESE",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 4,
+    "Cue": "METAL",
+    "Target": "GOLD",
+    "Paired": "METAL - GOLD",
+    "Q1": 1,
+    "Q2": 0,
+    "Q3": 1,
+    "Q4": 1
+  },
+  {
+    "": 5,
+    "Cue": "GREEN",
+    "Target": "APPLE",
+    "Paired": "GREEN - APPLE",
+    "Q1": 1,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 6,
+    "Cue": "PAPER",
+    "Target": "WOOD",
+    "Paired": "PAPER - WOOD",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 1
+  },
+  {
+    "": 7,
+    "Cue": "SMELL",
+    "Target": "FISH",
+    "Paired": "SMELL - FISH",
+    "Q1": 1,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 8,
+    "Cue": "HOLE",
+    "Target": "CIRCLE",
+    "Paired": "HOLE - CIRCLE",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 9,
+    "Cue": "LADY",
+    "Target": "BUG",
+    "Paired": "LADY - BUG",
+    "Q1": 1,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 10,
+    "Cue": "TRAIL",
+    "Target": "BLAZE",
+    "Paired": "TRAIL - BLAZE",
+    "Q1": 0,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 11,
+    "Cue": "HALL",
+    "Target": "DOOR",
+    "Paired": "HALL - DOOR",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 12,
+    "Cue": "LIQUOR",
+    "Target": "SCOTCH",
+    "Paired": "LIQUOR - SCOTCH",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 13,
+    "Cue": "ARMY",
+    "Target": "MARINE",
+    "Paired": "ARMY - MARINE",
+    "Q1": 0,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 14,
+    "Cue": "MUSCLE",
+    "Target": "BONE",
+    "Paired": "MUSCLE - BONE",
+    "Q1": 0,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 15,
+    "Cue": "ANGLE",
+    "Target": "CURVE",
+    "Paired": "ANGLE - CURVE",
+    "Q1": 1,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 16,
+    "Cue": "SUIT",
+    "Target": "JACKET",
+    "Paired": "SUIT - JACKET",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 17,
+    "Cue": "LIGHT",
+    "Target": "DAY",
+    "Paired": "LIGHT - DAY",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 18,
+    "Cue": "SUPPER",
+    "Target": "LUNCH",
+    "Paired": "SUPPER - LUNCH",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 19,
+    "Cue": "STUDY",
+    "Target": "DESK",
+    "Paired": "STUDY - DESK",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 20,
+    "Cue": "BLOOD",
+    "Target": "HOUND",
+    "Paired": "BLOOD - HOUND",
+    "Q1": 1,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 21,
+    "Cue": "POWDER",
+    "Target": "FLOUR",
+    "Paired": "POWDER - FLOUR",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 22,
+    "Cue": "HAND",
+    "Target": "WASH",
+    "Paired": "HAND - WASH",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 23,
+    "Cue": "HEART",
+    "Target": "CHEST",
+    "Paired": "HEART - CHEST",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 24,
+    "Cue": "SOUND",
+    "Target": "WAVE",
+    "Paired": "SOUND - WAVE",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 25,
+    "Cue": "LETTERS",
+    "Target": "NOTES",
+    "Paired": "LETTERS - NOTES",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 26,
+    "Cue": "BLUE",
+    "Target": "INK",
+    "Paired": "BLUE - INK",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 1
+  },
+  {
+    "": 27,
+    "Cue": "GROUND",
+    "Target": "SKY",
+    "Paired": "GROUND - SKY",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 28,
+    "Cue": "BRIGHT",
+    "Target": "YELLOW",
+    "Paired": "BRIGHT - YELLOW",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 29,
+    "Cue": "POCKET",
+    "Target": "KNIFE",
+    "Paired": "POCKET - KNIFE",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 30,
+    "Cue": "BAR",
+    "Target": "DRUNK",
+    "Paired": "BAR - DRUNK",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 31,
+    "Cue": "WINDOW",
+    "Target": "FRAME",
+    "Paired": "WINDOW - FRAME",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 32,
+    "Cue": "SPEAKER",
+    "Target": "GUEST",
+    "Paired": "SPEAKER - GUEST",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 33,
+    "Cue": "SPRING",
+    "Target": "WINTER",
+    "Paired": "SPRING - WINTER",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 34,
+    "Cue": "BIRD",
+    "Target": "ANIMAL",
+    "Paired": "BIRD - ANIMAL",
+    "Q1": 1,
+    "Q2": 0,
+    "Q3": 1,
+    "Q4": 1
+  },
+  {
+    "": 35,
+    "Cue": "BREATH",
+    "Target": "OXYGEN",
+    "Paired": "BREATH - OXYGEN",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 36,
+    "Cue": "TOOL",
+    "Target": "SHOVEL",
+    "Paired": "TOOL - SHOVEL",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 37,
+    "Cue": "STREET",
+    "Target": "BLOCK",
+    "Paired": "STREET - BLOCK",
+    "Q1": 0,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 38,
+    "Cue": "BALL",
+    "Target": "DANCE",
+    "Paired": "BALL - DANCE",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 39,
+    "Cue": "GENERAL",
+    "Target": "STORE",
+    "Paired": "GENERAL - STORE",
+    "Q1": 0,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 40,
+    "Cue": "SPEAK",
+    "Target": "LISTEN",
+    "Paired": "SPEAK - LISTEN",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 41,
+    "Cue": "MACHINE",
+    "Target": "ENGINE",
+    "Paired": "MACHINE - ENGINE",
+    "Q1": 0,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 42,
+    "Cue": "BEER",
+    "Target": "FOAM",
+    "Paired": "BEER - FOAM",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 43,
+    "Cue": "GRAVE",
+    "Target": "COFFIN",
+    "Paired": "GRAVE - COFFIN",
+    "Q1": 0,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 44,
+    "Cue": "BABY",
+    "Target": "BOTTLE",
+    "Paired": "BABY - BOTTLE",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 1
+  },
+  {
+    "": 45,
+    "Cue": "TALK",
+    "Target": "SING",
+    "Paired": "TALK - SING",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 46,
+    "Cue": "BRAIN",
+    "Target": "CELL",
+    "Paired": "BRAIN - CELL",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 47,
+    "Cue": "GIFT",
+    "Target": "BOX",
+    "Paired": "GIFT - BOX",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 48,
+    "Cue": "BAND",
+    "Target": "GROUP",
+    "Paired": "BAND - GROUP",
+    "Q1": 0,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 49,
+    "Cue": "PRISON",
+    "Target": "GUARD",
+    "Paired": "PRISON - GUARD",
+    "Q1": 0,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 50,
+    "Cue": "BEAR",
+    "Target": "CAVE",
+    "Paired": "BEAR - CAVE",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 51,
+    "Cue": "BROTHER",
+    "Target": "FAMILY",
+    "Paired": "BROTHER - FAMILY",
+    "Q1": 1,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 52,
+    "Cue": "SUN",
+    "Target": "HEAT",
+    "Paired": "SUN - HEAT",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 53,
+    "Cue": "BLIND",
+    "Target": "VISION",
+    "Paired": "BLIND - VISION",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 54,
+    "Cue": "SITE",
+    "Target": "CAMP",
+    "Paired": "SITE - CAMP",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 55,
+    "Cue": "AGE",
+    "Target": "WEIGHT",
+    "Paired": "AGE - WEIGHT",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 56,
+    "Cue": "PAINT",
+    "Target": "HOUSE",
+    "Paired": "PAINT - HOUSE",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 57,
+    "Cue": "ARM",
+    "Target": "CHAIR",
+    "Paired": "ARM - CHAIR",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 58,
+    "Cue": "STREAM",
+    "Target": "LAKE",
+    "Paired": "STREAM - LAKE",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 59,
+    "Cue": "MORNING",
+    "Target": "COFFEE",
+    "Paired": "MORNING - COFFEE",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 1
+  },
+  {
+    "": 60,
+    "Cue": "MEAT",
+    "Target": "LOAF",
+    "Paired": "MEAT - LOAF",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 1
+  },
+  {
+    "": 61,
+    "Cue": "LANE",
+    "Target": "ALLEY",
+    "Paired": "LANE - ALLEY",
+    "Q1": 0,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 62,
+    "Cue": "VEHICLE",
+    "Target": "TRUCK",
+    "Paired": "VEHICLE - TRUCK",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 63,
+    "Cue": "BOAT",
+    "Target": "SKI",
+    "Paired": "BOAT - SKI",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 64,
+    "Cue": "HIT",
+    "Target": "FIGHT",
+    "Paired": "HIT - FIGHT",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 65,
+    "Cue": "MANAGER",
+    "Target": "OWNER",
+    "Paired": "MANAGER - OWNER",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 66,
+    "Cue": "BEACH",
+    "Target": "TOWEL",
+    "Paired": "BEACH - TOWEL",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 1
+  },
+  {
+    "": 67,
+    "Cue": "GATE",
+    "Target": "LATCH",
+    "Paired": "GATE - LATCH",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 68,
+    "Cue": "BLANKET",
+    "Target": "PILLOW",
+    "Paired": "BLANKET - PILLOW",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 1,
+    "Q4": 0
+  },
+  {
+    "": 69,
+    "Cue": "PRINCE",
+    "Target": "CASTLE",
+    "Paired": "PRINCE - CASTLE",
+    "Q1": 0,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 70,
+    "Cue": "BEEF",
+    "Target": "CATTLE",
+    "Paired": "BEEF - CATTLE",
+    "Q1": 1,
+    "Q2": 0,
+    "Q3": 1,
+    "Q4": 1
+  },
+  {
+    "": 71,
+    "Cue": "MAIL",
+    "Target": "JUNK",
+    "Paired": "MAIL - JUNK",
+    "Q1": 1,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 72,
+    "Cue": "LAWYER",
+    "Target": "CASE",
+    "Paired": "LAWYER - CASE",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 73,
+    "Cue": "TEN",
+    "Target": "DIME",
+    "Paired": "TEN - DIME",
+    "Q1": 0,
+    "Q2": 1,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 74,
+    "Cue": "HIGHWAY",
+    "Target": "LANE",
+    "Paired": "HIGHWAY - LANE",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 0
+  },
+  {
+    "": 75,
+    "Cue": "MUSIC",
+    "Target": "NOISE",
+    "Paired": "MUSIC - NOISE",
+    "Q1": 0,
+    "Q2": 0,
+    "Q3": 0,
+    "Q4": 1
+  },
+  {
+    "": 76,
+    "Cue": "PLAY",
+    "Target": "KID",
+    "Paired": "PLAY - KID",
+    "Q1": 1,
+    "Q2": 0,
+    "Q3": 1,
+    "Q4": 0
+  }
+]
