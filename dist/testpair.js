bc40d5d5e87175a1892f70024056785b4f5b14eb
a3carranza <149541252+a3carranza@users.noreply.github.com>
2023-11-27 14:51:06 -0800

    Add files via upload

---
+++ dist/testpair.js
@@ -0,0 +1,121 @@
+var wordpair = [
+  {
+    Cue: "ham",
+    Target: "odd",
+    Q_1: "0",
+    Q_2: "1",
+    Q_3: "1",
+    Q_4: "1",
+    Q_5: "1",
+    Q_6: "1",
+    Paired: "ham - odd"
+  },
+  {
+    
+   Cue: "cat",
+    Target: "fry",
+    Q_1: "1",
+    Q_2: "1",
+    Q_3: "0",
+    Q_4: "1",
+    Q_5: "1",
+    Q_6: "0",
+    Paired: "cat - fry"
+  },
+  {
+    
+   Cue: "rat",
+    Target: "map",
+    Q_1: "1",
+    Q_2: "0",
+    Q_3: "0",
+    Q_4: "0",
+    Q_5: "1",
+    Q_6: "0",
+    Paired: "rat - map"
+  },
+  {
+    
+    Cue: "tad",
+    Target: "use",
+    Q_1: "0",
+    Q_2: "0",
+    Q_3: "0",
+    Q_4: "0",
+    Q_5: "0",
+    Q_6: "1",
+    Paired: "tad - use"
+  },
+  {
+    
+    Cue: "lad",
+    Target: "oar",
+    Q_1: "0",
+    Q_2: "0",
+    Q_3: "0",
+    Q_4: "0",
+    Q_5: "1",
+    Q_6: "0",
+    Paired: "lad - oar"
+  },
+  {
+   
+    Cue: "age",
+    Target: "nut",
+    Q_1: "0",
+    Q_2: "1",
+    Q_3: "0",
+    Q_4: "1",
+    Q_5: "1",
+    Q_6: "0",
+    Paired: "age - nut"
+  },
+  {
+    
+    Cue: "gel",
+    Target: "zap",
+    Q_1: "0",
+    Q_2: "1",
+    Q_3: "1",
+    Q_4: "0",
+    Q_5: "1",
+    Q_6: "0",
+    Paired: "gel - zap"
+  },
+  {
+    
+    Cue: "bad",
+    Target: "fox",
+    Q_1: "1",
+    Q_2: "0",
+    Q_3: "0",
+    Q_4: "0",
+    Q_5: "1",
+    Q_6: "1",
+    Paired: "bad - fox"
+  },
+  {
+    
+    Cue: "egg",
+    Target: "gas",
+    Q_1: "0",
+    Q_2: "1",
+    Q_3: "1",
+    Q_4: "1",
+    Q_5: "1",
+    Q_6: "0",
+    Paired: "egg - gas"
+  },
+  {
+   
+    Cue: "hen",
+    Target: "war",
+    Q_1: "1",
+    Q_2: "0",
+    Q_3: "0",
+    Q_4: "1",
+    Q_5: "1",
+    Q_6: "0",
+    Paired: "hen - war"
+  }
+]
